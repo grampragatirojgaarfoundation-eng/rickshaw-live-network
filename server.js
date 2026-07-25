0b6fad6fc14aa2c16e09156d8ff66bb90dee2a30
grampragatirojgaarfoundation-eng <grampragatirojgaarfoundation@gmail.com>
2026-07-25 18:09:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const path = require('path');
 const app = express();
 app.use(cors());
 
-// Serve static HTML file from the current directory
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
